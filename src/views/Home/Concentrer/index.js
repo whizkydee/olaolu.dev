9907061eb3aca689708f46c6aa9dc4f7c0d58c11
Olaolu Olawuyi <mrolaolu@gmail.com>
2019-06-07 14:02:34 +0100

    Working content for focus section

--- src/views/Home/Concentrer/index.js
+++ src/views/Home/Concentrer/index.js
@@ -7,13 +7,13 @@ const Concentrer = Vue.component('Concentrer', {
       <StyledConcentrer>
         <div class="concentrer__content">
           <Cavalier
-            heading="What am I?"
-            text="I like to call myself a UX Strategist and front end developer. I'm currently based in the city optimistic about things and smiling a lot."
+            heading="Design"
+            text="I'm probably not the typical designer positioned behind an Illustrator artboard adjusting pixels, but I design. Immersed in stylesheets tweaking font sizes and contemplating layouts is where you'll find me xo. I try my best to create the best experiences for users while staying fashionable."
           />
 
           <Cavalier
-            heading="Wanna know more?"
-            text="I like to call myself a UX Strategist and front end developer. I'm currently based in the city optimistic about things and smiling a lot."
+            heading="Engineering"
+            text="In JavaScript applications today, solving problems the most efficient way can be tricky as there's a thousand and one ways to go about them, which is why I'm equipped with the right tools and techniques to deliver fast, definitive solutions optimized for scalability &mdash; Web performance and extensibility are priorities on my radar."
           />
         </div>
       </StyledConcentrer>
