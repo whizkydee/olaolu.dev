7f69de1538d7d981168ef3b88ef43d439e0bac82
Olaolu Olawuyi <mrolaolu@gmail.com>
2019-06-07 11:57:39 +0100

    Improve legibility of deux__points text

--- src/views/Home/PitchSlate/styles.js
+++ src/views/Home/PitchSlate/styles.js
@@ -53,7 +53,7 @@ const StyledPitchSlate = styled.section`
   .deux__points {
     width: 500px;
     line-height: 1.5;
-    font-size: 0.95em;
+    font-size: 0.98em;
     justify-content: space-between;
     color: ${props => props.theme.colors.lime};
 
