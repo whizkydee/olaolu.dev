7883384e577d75a42fa158efef2ad3f1308fe63b
Olaolu Olawuyi <mrolaolu@gmail.com>
2019-06-08 03:32:53 +0100

    Re-word "Focus" section content

--- src/views/Home/Concentrer/index.js
+++ src/views/Home/Concentrer/index.js
@@ -8,12 +8,12 @@ const Concentrer = Vue.component('Concentrer', {
         <div class="concentrer__content">
           <Cavalier
             heading="Design"
-            text="I'm probably not the typical designer positioned behind an Illustrator artboard adjusting pixels, but I design. Immersed in stylesheets tweaking font sizes and contemplating layouts is where you'll find me xo. I try my best to create the best experiences for users while staying fashionable."
+            text="I'm probably not the typical designer positioned behind an Illustrator artboard adjusting pixels, but I design. Immersed in stylesheets tweaking font sizes and contemplating layouts is where you'll find me xo. I do my best to create fluent user experiences while staying fashionable."
           />
 
           <Cavalier
             heading="Engineering"
-            text="In JavaScript applications today, solving problems the most efficient way can be tricky as there's a thousand and one ways to go about them, which is why I'm equipped with the right tools and techniques to deliver fast, definitive solutions optimized for scalability &mdash; Web performance and extensibility are priorities on my radar."
+            text="In JavaScript applications these days where there's a thousand and one ways to go about solving a problem, I'm equipped with just the right tools and techniques to deliver fast, definitive solutions optimized for scalability &mdash; Web performance and extensibility are priorities on my radar."
           />
         </div>
       </StyledConcentrer>
