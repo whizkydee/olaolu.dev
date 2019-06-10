f88a68387caf6a47c5168c1214a303d500456efb
Olaolu Olawuyi <mrolaolu@gmail.com>
2019-06-10 22:13:07 +0100

    Improve cornerstone content

--- src/views/Home/Cornerstone/index.js
+++ src/views/Home/Cornerstone/index.js
@@ -6,15 +6,25 @@ const Cornerstone = Vue.component('Cornerstone', {
     return (
       <StyledCornerstone>
         <div class="cornerstone__content">
-          <Cavalier
-            heading="Design"
-            text="I'm probably not the typical designer positioned behind an Illustrator artboard adjusting pixels, but I design. Immersed in stylesheets tweaking font sizes and contemplating layouts is where you'll find me xo. I do my best to create fluent user experiences while staying fashionable."
-          />
+          <Cavalier heading="Design">
+            <p slot="text">
+              I'm probably not the typical designer positioned behind an
+              Illustrator artboard adjusting pixels, but I design. Immersed in
+              stylesheets tweaking font sizes and contemplating layouts is where
+              you'll find me (~_^). I do my best to create fluent user
+              experiences while staying fashionable.
+            </p>
+          </Cavalier>
 
-          <Cavalier
-            heading="Engineering"
-            text="In providing solutions to a problem, I'm equipped with just the right tools, and can absolutely function independently of them to deliver fast, definitive solutions optimized for scalability &mdash; Web performance and extensibility are priorities on my radar."
-          />
+          <Cavalier heading="Engineering">
+            <p slot="text">
+              In building applications and solving problems, I'm equipped with
+              just the right tools, and can absolutely function independently of
+              them to deliver fast, definitive solutions optimized for
+              scalability &mdash; Web performance and extensibility are
+              priorities on my radar.
+            </p>
+          </Cavalier>
         </div>
       </StyledCornerstone>
     )
