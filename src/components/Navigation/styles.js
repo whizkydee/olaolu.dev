bc877e4b3eb903e77fdca1227004f6f1e6e49acd
Olaolu Olawuyi <mrolaolu@gmail.com>
2019-07-13 12:53:42 +0100

    Navigation restyle

--- src/components/Navigation/styles.js
+++ src/components/Navigation/styles.js
@@ -6,15 +6,15 @@ const StyledNavigation = styled.nav`
   display: none;
   position: fixed;
   margin-top: -20px;
-  right: ${props => props.theme.header.padding};
   color: ${props => props.theme.colors['electric-blue']};
+  right: ${props => `calc(${props.theme.header.padding} + 1em)`};
 
   ${media.minWidth('medium', 1)`
     display: flex;
   `}
 
   ul {
-    width: 17px;
+    width: 0.4em;
     display: flex;
     flex-direction: column;
   }
@@ -27,36 +27,30 @@ const StyledNavigation = styled.nav`
     display: flex;
     outline: none;
     width: inherit;
-    min-height: 27px;
+    min-height: 2em;
     position: relative;
     color: currentColor;
     transition-delay: 100ms;
     outline-color: transparent;
     -webkit-tap-highlight-color: transparent;
 
-    &:not([aria-current='page']) {
-      margin: 0 0.5px;
-    }
-
     &:after {
       content: '';
-      width: 8px;
-      height: 8px;
+      width: 100%;
+      height: 0.4em;
       position: absolute;
-      border-radius: 100%;
-      transition: 0.2s 100ms;
-      clip-path: circle(100%);
+      transform: rotate(45deg);
       background-color: currentColor;
+      border: 1px solid currentColor;
+      transition: transform 0.2s 100ms;
     }
 
-    &[aria-current='page'] {
-      height: 30px;
-
-      &:after {
-        transform: scale(1.9);
-        background: transparent;
-        border: 1px solid currentColor;
-      }
+    &[aria-current='page']:after {
+      width: 0.76em;
+      height: 0.76em;
+      margin: 0 -3px;
+      transform: rotate(0);
+      background: transparent;
     }
   }
 `
