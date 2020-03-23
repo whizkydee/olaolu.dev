44d26de02a572c3073e7a7fad06d177eae17fee8
Olaolu Olawuyi <mrolaolu@gmail.com>
2020-03-23 11:25:55 +0100

    Simplify cross-site navigation logic

--- literal-sauce-drip/CrossSiteNav/index.vue
+++ literal-sauce-drip/CrossSiteNav/index.vue
@@ -1,7 +1,13 @@
 <template>
-  <ul class="cross-site-nav">
+  <ul class="cross-site-nav" v-if="isShelf">
+    <NavItem :href="landingURL">Home</NavItem>
+    <NavItem :href="workURL">My Work</NavItem>
+    <NavItem :href="resumeURL" external>My Résumé</NavItem>
+  </ul>
+
+  <ul class="cross-site-nav" v-else>
     <NavItem :href="landingURL" v-if="isWork">Home</NavItem>
-    <NavItem :href="workURL" v-else>Work</NavItem>
+    <NavItem :href="workURL" v-else>My Work</NavItem>
     <NavItem :href="shelfURL" v-if="isHome || isWork">My Shelf</NavItem>
     <NavItem :href="landingURL" v-else>Home</NavItem>
     <NavItem :href="resumeURL" external>My Résumé</NavItem>
