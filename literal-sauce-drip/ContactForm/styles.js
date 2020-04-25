ee5e1a358088014c3cceb874e870df1cb33f15a5
Olaolu Olawuyi <mrolaolu@gmail.com>
2020-04-25 23:09:47 +0100

    (chore): Cleanup

--- literal-sauce-drip/ContactForm/styles.js
+++ literal-sauce-drip/ContactForm/styles.js
@@ -1,90 +1,88 @@
 import { media } from '../helpers'
 import styled from 'vue-styled-components'
 
-const StyledContactForm = styled.form`
-  display: flex;
-  position: relative;
-  flex-direction: column;
-
-  .form-row {
-    width: 100%;
+export default Object.assign(
+  styled.form`
     display: flex;
     position: relative;
-    justify-content: space-between;
+    flex-direction: column;
 
-    ${media.maxWidth('portrait')`
-      font-size: 1.2rem;
-      flex-direction: column;
+    .form-row {
+      width: 100%;
+      display: flex;
+      position: relative;
+      justify-content: space-between;
 
-      .input-group:first-of-type {
+      ${media.maxWidth('portrait')`
+        font-size: 1.2rem;
+        flex-direction: column;
+
+        .input-group:first-of-type {
+          margin-bottom: 3rem;
+        }
+      `}
+
+      ${media.minWidth('>portrait')`
+        &:first-of-type .input-group {
+          width: 45%;
+        }
+      `}
+
+      &:not(:last-of-type) {
         margin-bottom: 3rem;
       }
-    `}
-
-    ${media.minWidth('>portrait')`
-      &:first-of-type .input-group {
-        width: 45%;
-      }
-    `}
-
-    &:not(:last-of-type) {
-      margin-bottom: 3rem;
-    }
-  }
-
-  .input-group {
-    color: rgba(61, 21, 95, 0.5);
-
-    &:not(.focused) {
-      textarea,
-      input:not([type='submit']):not([type='button']) {
-        border-color: currentColor;
-      }
     }
 
-    &.focused {
-      input,
-      textarea {
-        &::placeholder {
-          color: transparent;
+    .input-group {
+      color: rgba(61, 21, 95, 0.5);
+
+      &:not(.focused) {
+        textarea,
+        input:not([type='submit']):not([type='button']) {
+          border-color: currentColor;
+        }
+      }
+
+      &.focused {
+        input,
+        textarea {
+          &::placeholder {
+            color: transparent;
+          }
         }
       }
     }
-  }
 
-  input:-webkit-autofill {
-    -webkit-box-shadow: 0 0 0 100px #f9f9f9 inset !important;
-  }
-
-  textarea,
-  input:not([type='submit']):not([type='button']) {
-    line-height: 1.5;
-    font-size: 1.15em;
-
-    &::placeholder {
-      transition: color 0.1s ease-out;
+    input:-webkit-autofill {
+      -webkit-box-shadow: 0 0 0 100px #f9f9f9 inset !important;
     }
-  }
 
-  textarea {
-    height: 4.2em;
+    textarea,
+    input:not([type='submit']):not([type='button']) {
+      line-height: 1.5;
+      font-size: 1.15em;
 
-    ${media.maxWidth('portrait')`
-      height: 6em;
-    `}
-  }
+      &::placeholder {
+        transition: color 0.1s ease-out;
+      }
+    }
 
-  #submit-button {
-    margin-top: 5.2em;
+    textarea {
+      height: 4.2em;
 
-    ${media.maxWidth('portrait')`
-      margin-top: 0;
-      align-self: flex-start;
-    `}
-  }
-`
+      ${media.maxWidth('portrait')`
+        height: 6em;
+      `}
+    }
 
-export default {
-  ...StyledContactForm,
-  name: 'StyledContactForm',
-}
+    #submit-button {
+      margin-top: 5.2em;
+
+      ${media.maxWidth('portrait')`
+        margin-top: 0;
+        align-self: flex-start;
+      `}
+    }
+  `,
+  { name: 'StyledContactForm' }
+)
